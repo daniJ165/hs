30ec9e245572d7899b8039ee97e4e73b9d7a2e4c
dj <djubea@proton.me>
2025-08-24 13:17:44 -0400

    first commit to hs_random branch

--- hs.py
+++ hs.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python3
 
+import random
+
 def happy_sabbath():
-    print("cheers mate happy sabbath !!")
+    hs_options = [ "cheers mate happy sabbath !!", "happy sabbath !", "appy abbath", "ppy bbath", "HaPpY SaBbAtH" ]
+    random_choice = random.choice(hs_options)
+    print(random_choice)
 
 happy_sabbath()
