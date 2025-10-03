8cceadca3822e4ac8b0dd8c8ac7dc67d444656f6
dj <djubea@proton.me>
2025-10-03 13:53:12 -0400

    added dvorak

--- hs.py
+++ hs.py
@@ -3,7 +3,7 @@
 import random
 
 def happy_sabbath():
-    hs_options = [ "cheers mate happy sabbath !!", "happy sabbath goose !", "dnalel htabbas yppah", "happy Sabbathmate mate!", "happpyyyy sabbaaatthhhh lelanddddd!!", "yooo lessgooo u got thru another week!! happy sabbath", "IT'S SABBATH LESSSGOOOO! HAPPY SABBATH MATE!", "happy sabbath leland -- c ya at ss (hopefully)", "sabbath happy !", "have a happy, wonderful, blessed, restful, beautiful, awesome, sabbath!", "HaPpY SaBbAtH", "h/|ppy $a88a+h $i!!y g00$e", "abcdefG(oose)H(appy)ijklmN(dia)opqrS(abbath/seychelles)tuvwxyz" ]
+    hs_options = [ "cheers mate happy sabbath !!", "happy sabbath goose !", "dnalel htabbas yppah", "happy Sabbathmate mate!", "happpyyyy sabbaaatthhhh lelanddddd!!", "yooo lessgooo u got thru another week!! happy sabbath", "IT'S SABBATH LESSSGOOOO! HAPPY SABBATH MATE!", "happy sabbath leland -- c ya at ss", "sabbath happy !", "dallf oaxxayd n.nabe! dak. ugb e.jrecbi ydco m.ooai.", "HaPpY SaBbAtH", "h/|ppy $a88a+h $i!!y g00$e", "abcdefG(oose)H(appy)ijklmN(dia)opqrS(abbath/seychelles)tuvwxyz" ]
 
     # bruh why u reading da code?? 
 
