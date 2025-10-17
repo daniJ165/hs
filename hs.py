1b9fde64d86c0688ccb50c3fd874e46943172443
dj <djubea@proton.me>
2025-10-17 16:07:23 -0400

    call

--- hs.py
+++ hs.py
@@ -3,12 +3,15 @@
 import random
 
 def happy_sabbath():
-    hs_options = [ "cheers mate happy sabbath !!", "happy sabbath goose !", "dnalel htabbas yppah", "happy Sabbathmate mate!", "happpyyyy sabbaaatthhhh lelanddddd!!", "yooo lessgooo u got thru another week!! happy sabbath", "IT'S SABBATH LESSSGOOOO! HAPPY SABBATH MATE!", "happy sabbath leland -- c ya at ss", "sabbath happy !", "dallf oaxxayd n.nabe! dak. ugb e.jrecbi ydco m.ooai.", "HaPpY SaBbAtH", "h/|ppy $a88a+h $i!!y g00$e", "abcdefG(oose)H(appy)ijklmN(dia)opqrS(abbath/seychelles)tuvwxyz" ]
+    #hs_options = [ "cheers mate happy sabbath !!", "happy sabbath goose !", "dnalel htabbas yppah", "happy Sabbathmate mate!", "happpyyyy sabbaaatthhhh lelanddddd!!", "yooo lessgooo u got thru another week!! happy sabbath", "IT'S SABBATH LESSSGOOOO! HAPPY SABBATH MATE!", "happy sabbath leland -- c ya at ss", "sabbath happy !", "dallf oaxxayd n.nabe! dak. ugb e.jrecbi ydco m.ooai.", "HaPpY SaBbAtH", "h/|ppy $a88a+h $i!!y g00$e", "abcdefG(oose)H(appy)ijklmN(dia)opqrS(abbath/seychelles)tuvwxyz" ]
 
-    # bruh why u reading da code?? 
+    
+    print ("call me")
 
-    random_choice = random.choice(hs_options)
-    print(random_choice)
+   # random_choice = random.choice(hs_options)
+   # print(random_choice)
+
+
 
         
 
