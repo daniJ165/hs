10801ab17bea82d5ab859af06816e768555fe0d3
dj <djubea@proton.me>
2025-11-21 21:48:13 -0500

    nov33u

--- hs.sh
+++ hs.sh
@@ -18,26 +18,22 @@ sleep 2
 
 clear
 
-echo " there's so much i want to say, but i know saying it in person rather than on the terminal is much more valuable."
-
-sleep 5
-
-clear
 
 
-echo "but rn i just want to say that i'm so thankful to have a friend that is always there for me and prays for me and draws me closer to GOD !"
-sleep 5
+
+echo "i just want to say that i'm so thankful to have a friend that is always there for me, prays for me, and helps me to draw closer to God"
+sleep 6
 clear
 
 echo "i'm so glad that you will be there at my baptism to witness my commitment to serve GOD for the rest of my life!"
-sleep 5
+sleep 6
 clear
 echo " "
 sleep 2
 clear
 
 
-echo "i'll see you soon, happy sabbath mate!!"
+echo "i'll see you in ss, happy sabbath mate!!"
 
 sleep 3
 
