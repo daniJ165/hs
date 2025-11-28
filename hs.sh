ffe13c6bdda2928b48ea7f04cafa4148c088425c
dj <djubea@proton.me>
2025-11-28 18:25:27 -0500

    bash

--- hs.sh
+++ hs.sh
@@ -1,41 +1,21 @@
 #! /bin/bash
 
-echo " "
-
-sleep 2
-
-clear
-
-echo "THE DAY IS FINALLY HERE!!! "
-
-sleep 3 
-
-clear
 
 echo " "
 
-sleep 2
+sleep 2 
 
 clear
 
+echo "shucks i forgot to spend some time during the week to make something interesing, so ig it's another simple bash script this week"
 
-
-
-echo "i just want to say that i'm so thankful to have a friend that is always there for me, prays for me, and helps me to draw closer to God"
-sleep 6
-clear
-
-echo "i'm so glad that you will be there at my baptism to witness my commitment to serve GOD for the rest of my life!"
-sleep 6
-clear
-echo " "
-sleep 2
-clear
-
-
-echo "i'll see you in ss, happy sabbath mate!!"
-
-sleep 3
+sleep 5 
 
 clear
+
+echo "that was so great to hang out wit ya, i'll c ya again soon, cheers mate happy sabbath!!"
+
+sleep 4
+
+clear 
 
