2f57bb5382a3078812575c9547c21bac858daaca
dj <djubea@proton.me>
2025-12-20 00:37:26 -0500

    nyc lessgooo im with mo

--- default.nix
+++ default.nix
@@ -16,7 +16,7 @@ pkgs.stdenv.mkDerivation rec {
 
     installPhase = ''
       mkdir -p $out/bin
-      cp $src/lee.sh $out/bin/hs
+      cp $src/hs.sh $out/bin/hs
       chmod +x $out/bin/hs
       '';
 }
