349fb6763265e2a487f50080aa391e083d60d194
dj <djubea@proton.me>
2025-12-26 19:30:06 -0500

    psalm 91

--- hs.sh
+++ hs.sh
@@ -7,34 +7,40 @@ sleep 2
 
 clear
 
-rows=$(tput lines)
-cols=$(tput cols)
-
-message_height=2
-start_row=$(( (rows - message_height) / 2 ))
-
-message1="from mo - 'Hey Leland! It was a pleasure to finally meet you. Happy Sabbath! Keep taking good care of your beautiful lady for me. And as you guys say, cheers!'"
-
-message2=" from ur lady: morningymorning & cheers mate happy sabbath !! it's so blarg i won't see you today, but i pray that GOD fills your heart with \x1b[48;5;166mpeace\x1b[0m and \x1b[48;2;194;21;43mjoy\x1b[0m today. \x1b[48;2;194;21;139mlove you!\x1b[0m"
-
-     start_row1=${start_row} 
-     start_row2=$((start_row1 + 1))
-
-     col1=$(( (cols - ${#message1}) / 2 ))
-     col2=$(( (cols - ${#message2}) / 2 ))
 
 
-tput cup $start_row1 $col1
+
+message1="\x1b[38;5;196mHe who dwells in the secret place of the Most High
+Shall abide under the shadow of the Almighty.\x1b[0m
+ 
+\x1b[38;5;197mI will say of the Lord, 'He is my refuge and my fortress;
+My God, in Him I will trust.'\x1b[0m
+ 
+\x1b[38;5;198mSurely He shall deliver you from the snare of the fowler
+And from the perilous pestilence.\x1b[0m
+ 
+\x1b[38;5;205mHe shall cover you with His feathers,
+And under His wings you shall take refuge;\x1b[0m
+
+\x1b[38;5;204mHis truth shall be your shield and buckler.\x1b[0m
+
+
+
+
+\x1b[38;5;211mthis is a section from psalm 91, one of my favorite psalms 
+(there are so many i cant pick a favorite lol)\x1b[0m 
+
+\x1b[38;5;217mi read it recently and decided to share it with you!\x1b[0m
+
+\x1b[38;5;217mcheers mate happy sabbath !!!\x1b[0m"
+
+
         echo -e "${message1}"
 
-sleep 7
+sleep 10
 
 echo " "
 sleep 1
 
 clear
 
-tput cup $start_row1 $col1
-        echo -e "${message2}"
-sleep 7
-clear
