7a1bc98b2615527ecc717328e3705238aebe42db
dj <djubea@proton.me>
2026-01-03 07:27:59 -0500

    gyc

--- hs.sh
+++ hs.sh
@@ -7,40 +7,12 @@ sleep 2
 
 clear
 
+echo "it was great romping around gyc wit ya. cheers mate happy sabbath!!"
 
-
-
-message1="\x1b[38;5;196mHe who dwells in the secret place of the Most High
-Shall abide under the shadow of the Almighty.\x1b[0m
- 
-\x1b[38;5;197mI will say of the Lord, 'He is my refuge and my fortress;
-My God, in Him I will trust.'\x1b[0m
- 
-\x1b[38;5;198mSurely He shall deliver you from the snare of the fowler
-And from the perilous pestilence.\x1b[0m
- 
-\x1b[38;5;205mHe shall cover you with His feathers,
-And under His wings you shall take refuge;\x1b[0m
-
-\x1b[38;5;204mHis truth shall be your shield and buckler.\x1b[0m
+sleep 5 
 
 
 
 
-\x1b[38;5;211mthis is a section from psalm 91, one of my favorite psalms 
-(there are so many i cant pick a favorite lol)\x1b[0m 
 
-\x1b[38;5;217mi read it recently and decided to share it with you!\x1b[0m
-
-\x1b[38;5;217mcheers mate happy sabbath !!!\x1b[0m"
-
-
-        echo -e "${message1}"
-
-sleep 10
-
-echo " "
-sleep 1
-
-clear
 
