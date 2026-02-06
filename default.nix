a5dea3c254950aed1a970fea7655c5fb0d6e8b27
dj <djubea@proton.me>
2026-02-06 17:06:07 -0500

    asdf defaut

--- default.nix
+++ default.nix
@@ -16,7 +16,7 @@ pkgs.stdenv.mkDerivation rec {
 
     installPhase = ''
       mkdir -p $out/bin
-      cp $src/cheers.sh $out/bin/hs
+      cp $src/asdf.sh $out/bin/hs
       chmod +x $out/bin/hs
       '';
 }
